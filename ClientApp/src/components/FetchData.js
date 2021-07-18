64663d0f4c0c3d9d9e4f13362a0bddd2620c9319
Larry Wells <lgwells1@me.com>
2021-07-18 00:17:22 -0500

    updates to support new components and functionality. sorry future me for more details, forgot to commit as I modified each ittem

--- ClientApp/src/components/FetchData.js
+++ ClientApp/src/components/FetchData.js
@@ -14,46 +14,53 @@ export class FetchData extends Component {
 
   static renderForecastsTable(forecasts) {
     return (
-      <table className='table table-striped' aria-labelledby="tabelLabel">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Temp. (C)</th>
-            <th>Temp. (F)</th>
-            <th>Summary</th>
-          </tr>
-        </thead>
-        <tbody>
-          {forecasts.map(forecast =>
-            <tr key={forecast.date}>
-              <td>{forecast.date}</td>
-              <td>{forecast.temperatureC}</td>
-              <td>{forecast.temperatureF}</td>
-              <td>{forecast.summary}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+      <div>
+        <p>
+        {forecasts[0].name}
+        </p>
+      
+      </div>
+
+      // <table className='table table-striped' aria-labelledby="tabelLabel">
+      //   <thead>
+      //     <tr>
+      //       <th>Date</th>
+      //       <th>Temp. (C)</th>
+      //       <th>Temp. (F)</th>
+      //       <th>Summary</th>
+      //     </tr>
+      //   </thead>
+      //   <tbody>
+      //     {forecasts.map(forecast =>
+      //       <tr key={forecast.date}>
+      //         <td>{forecast.date}</td>
+      //         <td>{forecast.temperatureC}</td>
+      //         <td>{forecast.temperatureF}</td>
+      //         <td>{forecast.summary}</td>
+      //       </tr>
+      //     )}
+      //   </tbody>
+      // </table>
     );
   }
 
   render() {
-    let contents = this.state.loading
-      ? <p><em>Loading...</em></p>
-      : FetchData.renderForecastsTable(this.state.forecasts);
+
 
     return (
       <div>
-        <h1 id="tabelLabel" >Weather forecast</h1>
+        {/* <h1 id="tabelLabel" >Weather forecast</h1>
         <p>This component demonstrates fetching data from the server.</p>
-        {contents}
+        {contents} */}
       </div>
     );
   }
 
   async populateWeatherData() {
-    const response = await fetch('weatherforecast');
+    const response = await fetch('product');
+
     const data = await response.json();
     this.setState({ forecasts: data, loading: false });
+    console.log(data);
   }
 }
--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -1,25 +1,92 @@
 import React, { Component } from 'react';
+import ProductList from './products/ProductList';
+import Cart from './Cart'
 
 export class Home extends Component {
   static displayName = Home.name;
 
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      products: [], 
+      cart: {},
+      cartVisible: false,
+      loading: true };
+  }
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  showCart = event => {
+    this.setState( { cartVisible: !this.state.cartVisible });
+  };
+
+  addToCart = item => {
+    let cart = this.state.cart;
+    if (cart[item.id]) {
+      //Item exists already, increment quantity
+      cart[item.id].amount += item.amount;
+    }
+    else {
+      //Add item to cart
+      cart[item.id] = item;
+    }
+    console.log("ADDING TO CART");
+    console.log(cart);
+    this.setState({ cart });
+  };
+
+  removeFromCart = itemId => {
+    let cart = this.state.cart;
+
+    delete cart[itemId];
+
+    this.setState({ cart });
+  };
+
+  emptyCart = () => {
+    let cart = {};
+    this.setState({ cart });
+  };
+
+  async getProducts() {
+    const response = await fetch('product');
+
+    const data = await response.json();
+    this.setState({ products: data, loading: false });
+    console.log(data);
+  }
+
+  render() {
+    let contents = this.state.loading
+    ? <p><em>Loading...</em></p>
+    : <ProductList 
+        products={this.state.products}
+        addToCart={this.addToCart}/>
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+        <h1>Welcome to Kwik-E-Mart</h1>
+        <div>
+          { contents }
+        </div>
+        <div>
+          <button
+            onClick={e => {
+              this.showCart(e);
+            }}>
+          Cart
+          </button>
+        </div>
+        <div>
+          <Cart
+          emptyCart={this.emptyCart}
+          removeFromCart={this.removeFromCart}
+          cart={this.state.cart}
+          onClose={this.showCart}
+          show={this.state.cartVisible} 
+          />
+        </div>
       </div>
     );
   }
