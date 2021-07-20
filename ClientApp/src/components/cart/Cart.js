d26f483452d9dfce81da05b17ceb83f2f213972e
Larry Wells <lgwells1@me.com>
2021-07-19 21:43:00 -0500

    fixed rogue traveling total value in cart, now fixed to bottom left

--- ClientApp/src/components/cart/Cart.js
+++ ClientApp/src/components/cart/Cart.js
@@ -15,65 +15,68 @@ const Cart = props => {
     getTotal(cart);
   });
 
-   const getTotal = () => {
+  const getTotal = () => {
     let total = 0;
     for (var key in cart) {
-        total += parseFloat((cart[key].amount * cart[key].product.price).toFixed(2));
+      total += parseFloat((cart[key].amount * cart[key].product.price));
     }
-    
-    setTotalAmount(total);
+
+    setTotalAmount(total.toFixed(2));
   }
 
   return (
-      !props.show
+    !props.show
       ? <></>
       :
-    <>
-      <div className="hero is-primary">
-        <div className="hero-body container">
-          <h4 className="title">My Cart</h4>
+      <>
+        <div className="hero is-primary">
+          <div className="hero-body container">
+            <h4 className="title">My Cart</h4>
+          </div>
         </div>
-      </div>
-      <br />
-      <div className="container">
-        {cartKeys.length ? (
-          <div className="column columns is-multiline">
-            {cartKeys.map(key => (
-              <CartItem
-                cartKey={key}
-                key={key}
-                cartItem={cart[key]}
-                removeFromCart={props.removeFromCart}
-              />
-            ))}
-            <div>
-                <p>Total (Before Tax): ${ totalAmount }</p>
-            </div>
-            <div className="column is-12 is-clearfix">
-              <br />
-              <div className="is-pulled-right">
-                <button
-                  onClick={props.emptyCart}
-                  className="button is-warning "
-                >
-                  Clear cart
-                </button>{" "}
-                <button
-                  className="button is-success"
-                  onClick={props.showReceipt}
-                >
-                  Checkout
-                </button>
+        <br />
+        <div className="container">
+          {cartKeys.length ? (
+            <div className="column columns is-multiline">
+              {cartKeys.map(key => (
+                <CartItem
+                  cartKey={key}
+                  key={key}
+                  cartItem={cart[key]}
+                  removeFromCart={props.removeFromCart}
+                />
+              ))}
+              <div className="column is-12 is-clearfix">
+                <br />
+                <div className="is-pulled-left">
+                  <p>Total (Before Tax): ${totalAmount}</p>
+                </div>
+              </div>
+              <div className="column is-12 is-clearfix">
+                <br />
+                <div className="is-pulled-right">
+                  <button
+                    onClick={props.emptyCart}
+                    className="button is-warning "
+                  >
+                    Clear cart
+                  </button>{" "}
+                  <button
+                    className="button is-success"
+                    onClick={props.showReceipt}
+                  >
+                    Checkout
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        ) : (
-          <div className="column">
-            <div className="title has-text-grey-light">No item in cart!</div>
-          </div>
-        )}
-      </div>
-    </>
+          ) : (
+            <div className="column">
+              <div className="title has-text-grey-light">No item in cart!</div>
+            </div>
+          )}
+        </div>
+      </>
   );
 };
 
